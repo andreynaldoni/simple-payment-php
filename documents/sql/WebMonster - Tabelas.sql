384ba9dcdd8246c05fd2988f827e5bc02cca1900
Yunes Noronha Pereira da Silva <yunesnoronha@gmail.com>
2016-03-26 13:03:37 -0300

    Subindo arquivo

--- documents/sql/WebMonster - Tabelas.sql
+++ documents/sql/WebMonster - Tabelas.sql
@@ -1,8 +1,10 @@
+--CRIAÇÃO DAS TABELAS
+
 CREATE TABLE tb_produto
 (
     cd_produto INTEGER NOT NULL AUTO_INCREMENT,
-    nm_produto VARCHAR(50) NOT NULL,
-    ds_produto VARCHAR(50) NOT NULL,
+    nm_produto VARCHAR(60) NOT NULL,
+    ds_produto VARCHAR(100) NOT NULL,
     vl_produto DECIMAL(10,2) NOT NULL,
     qt_produto INTEGER NOT NULL,
     PRIMARY KEY (cd_produto)
@@ -12,26 +14,62 @@ CREATE TABLE tb_ingrediente
 (
     cd_ingrediente INTEGER NOT NULL AUTO_INCREMENT,
     nm_ingrediente VARCHAR(50) NOT NULL,
+    ds_ingrediente VARCHAR(100),
+    qt_ingrediente INTEGER,
+    vl_ingrediente DECIMAL(10,2),
     PRIMARY KEY(cd_ingrediente)
-);
-
-CREATE TABLE tb_pedido
-(
-    cd_pedido INTEGER NOT NULL AUTO_INCREMENT,
-    vl_total_pedido DECIMAL(10,2) NOT NULL,
-    dt_emissao DATETIME,
-    PRIMARY KEY(cd_pedido)
 );
 
 CREATE TABLE cliente
 (
     cd_cliente INTEGER NOT NULL AUTO_INCREMENT,
-    nm_cliente VARCHAR(50) NOT NULL,
+    nm_cliente VARCHAR(60) NOT NULL,
+    nm_sobrenome VARCHAR(100),
+    cd_ddd INTEGER,
+    cd_telefone INTEGER,
+    ic_tipo_documento VARCHAR(2),
+    cd_cpf VARCHAR(15),
+    cd_cnpj VARCHAR(15),
+    nm_pais VARCHAR(15),
+    nm_estado VARCHAR(2),
+    nm_cidade VARCHAR(60),
+    cd_cep VARCHAR(8),
+    nm_bairro VARCHAR(60),
+    nm_rua    VARCHAR(80),
+    cd_numero INTEGER,
+    ds_complemento VARCHAR(40),
     nm_email_cliente VARCHAR(100),
     cd_cartao_cliente VARCHAR(30),
     cd_operadora_cartao VARCHAR(30),
+    dt_validade_cartao DATE,
     PRIMARY KEY(cd_cliente)
 );
+
+CREATE TABLE tb_pedido
+(
+    cd_pedido INTEGER NOT NULL AUTO_INCREMENT,
+    dt_emissao DATETIME,
+    vl_total_pedido DECIMAL(10,2) NOT NULL,    
+    ic_cancelado CHAR(2),
+    qt_total_parcela INTEGER,
+    PRIMARY KEY(cd_pedido)
+);
+
+CREATE TABLE tb_pedido_parcela 
+(
+    cd_pedido INTEGER NOT NULL AUTO_INCREMENT,
+    dt_emissao DATETIME,
+    cd_parcela INTEGER,
+    vl_parcela DECIMAL(10,2),
+    PRIMARY KEY(cd_pedido)
+)
+
+CREATE TABLE tb_transacao
+(
+    
+)
+
+--CRIAÇÃO DAS TABELAS DE RESOLUÇÃO
 
 CREATE TABLE ingrediente_produto
 (
