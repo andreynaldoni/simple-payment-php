ffa17ee018db2fed8d63782c8d7e4c6c8dbd0094
Andrey Naldoni <andreynaldoni@gmail.com>
2016-03-28 22:03:08 -0300

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 	define('DB_HOSTNAME','localhost');
 	define('DB_USERNAME','root');
-	define('DB_PASSWORD','password');
-	define('DB_DATABASE','db_simple_payment');
-	define('DB_PREFIX','wm');
+	define('DB_PASSWORD','password'); //Your Password
+	define('DB_DATABASE','db_simple_payment'); //Your Database
+	define('DB_PREFIX','tb');
 	define('DB_CHARSET','utf8');
 ?>
