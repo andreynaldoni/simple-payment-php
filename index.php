583cfec2367757513d2b6fedc78596efdb469f28
Paulo Barros <paulo.sb@live.com>
2016-03-29 14:16:49 -0300

    Testing changes

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 <?php
 	require 'config.php';
 	require 'connection.php';
-    require './dao/clienteDAO.php';
+    require 'dao/clienteDAO.php';
 
     $clienteDAO = new ClienteDAO();
     $clientes = $clienteDAO->listCliente();
