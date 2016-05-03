0651b42f8e3db81412949dc39ba7b7523b6f4962
Yunes Noronha Pereira da Silva <yunesnoronha@gmail.com>
2016-05-02 22:14:51 -0300

    mudança de diretorios

--- views/cliente/index.php
+++ views/cliente/index.php
@@ -1,7 +1,7 @@
 <?php
     //DAO->Cliente
    //include "dao/clienteDAO.php";
-    include_once "negocio/clienteNeg.php";
+    include_once "business/clienteNeg.php";
     include "models/cliente.php";    
 ?>
 
