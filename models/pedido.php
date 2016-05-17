1e099b2b34ff48f6a36fd8e552a509900f7c4faf
Andrey Henriques Naldoni <andreynaldoni@gmail.com>
2016-05-17 11:32:28 -0300

    Pedido / Produto Pedido model

--- models/pedido.php
+++ models/pedido.php
@@ -2,7 +2,11 @@
 
     class Pedido{
         private $cd_pedido;
-        private $vl_pedido;
+        private $dt_emissao;
+        private $vl_total_pedido;
+        private $ic_cancelado;
+        private $qt_total_parcela;
+        private $cd_cliente;
         
         public function getCdPedido(){
             return $this->cd_pedido;
@@ -12,22 +16,54 @@
             $this->cd_pedido = $cd_pedido;
         }
         
-        public function getVlPedido(){
-            return $this->vl_pedido;
+        public function getDtEmissao(){
+            return $this->dt_emissao;
         }
         
-        public function setVlPedido($vl_pedido){
-            $this->vl_pedido = $vl_pedido;
+        public function setDtEmissao($dt_emissao){
+            $this->dt_emissao = $dt_emissao;
         }
         
-        public function addVlPedido($value){
-            $this->vl_pedido += $value;
+        public function getVlTotalPedido(){
+            return $this->vl_total_pedido;
+        }
+        
+        public function setVlTotalPedido($vl_total_pedido){
+            $this->vl_total_pedido = $vl_total_pedido;
+        }
+        
+        public function getIcCancelado(){
+            return $this->ic_cancelado;
+        }
+        
+        public function setIcCancelado($ic_cancelado){
+            $this->ic_cancelado = $ic_cancelado;
+        }
+        
+        public function getQtTotalParcela(){
+            return $this->qt_total_parcela;
+        }
+        
+        public function setQtTotalParcela($qt_total_parcela){
+            $this->qt_total_parcela = $qt_total_parcela;
+        }
+        
+        public function getCdCliente(){
+            return $this->cd_cliente;
+        }
+        
+        public function setCdCliente($cd_cliente){
+            $this->cd_cliente = $cd_cliente;
         }
         
         public function getPedido(){
             return array(
-              "cd_pedido" => $this->cd_pedido,
-              "vl_pedido" => $this->vl_pedido  
+                "cd_pedido"        => $this->cd_pedido,
+                "dt_emissao"       => $this->dt_emissao,
+                "vl_pedido"        => $this->vl_pedido,
+                "ic_cancelado"     => $this->ic_cancelado,
+                "qt_total_parcela" => $this->qt_total_parcela,
+                "cd_cliente"       => $this->cd_cliente
             );
         }
     }
--- models/produtopedido.php
+++ models/produtopedido.php
@@ -1,15 +1,19 @@
 <?php 
 
     class ProdutoPedido{
-        private $cd_produto;
+        private $cd_produto_pedido;
         private $cd_pedido;
+        private $cd_produto;
+        private $vl_total_produto_pedido;
+        private $ds_obs;
+        private $qt_produto_pedido;
         
-        public function getCdProduto(){
-            return $this->cd_produto;
+        public function getCdProdutoPedido(){
+            return $this->cd_produto_pedido;
         }
         
-        public function setCdProduto($cd_produto){
-            $this->cd_produto = $cd_produto;
+        public function setCdProdutoPedido($cd_produto_pedido){
+            $this->cd_produto_pedido = $cd_produto_pedido;
         }
 
         public function getCdPedido(){
@@ -20,10 +24,38 @@
             $this->cd_pedido = $cd_pedido;
         }
         
+        public function getCdProduto(){
+            return $this->cd_produto;
+        }
+        
+        public function setCdProduto($cd_produto){
+            $this->cd_produto = $cd_produto;   
+        }
+        
+        public function getTotalProdutoPedido(){
+            return $this->vl_total_produto_pedido;
+        }
+        
+        public function setTotalProdutoPedido($vl_total_produto_pedido){
+            $this->vl_total_produto_pedido = $vl_total_produto_pedido;
+        }
+        
+        public function getQtProdutoPedido(){
+            return $this->qt_produto_pedido;
+        }
+        
+        public function setQtProdutoPedido($qt_produto_pedido){
+            $this->qt_produto_pedido = $qt_produto_pedido;
+        }
+        
         public function getProdutoPedido(){
             return array(
-                "cd_produto"    => $this->cd_produto,
-                "cd_pedido"     => $this->cd_pedido
+                "cd_produto_pedido"       => $this->cd_produto_pedido,
+                "cd_pedido"               => $this->cd_pedido,
+                "cd_produto"              => $this->cd_produto,
+                "vl_total_produto_pedido" => $this->vl_total_produto_pedido,
+                "ds_obs"                  => $this->ds_obs,
+                "qt_produto_pedido"       => $this->qt_produto_pedido,
             );
         }
     }
