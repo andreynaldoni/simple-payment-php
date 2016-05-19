47d5f2df8edb95fb6f4b88e84245e1f00ab90f8e
Andrey Henriques Naldoni <andreynaldoni@gmail.com>
2016-05-19 16:17:09 -0300

    Grid Pedidos

--- views/pedido/index.php
+++ views/pedido/index.php
@@ -4,8 +4,53 @@
     if(!admin_check()){
         redirect('/');
     }
+    
+    $pedidoNeg = new PedidoNeg();
+    $pedidos = $pedidoNeg->getPedido();
 ?>
-    <h3 class="text-center">Tabela de Produtos</h3>
     <div class="container">
-        <h4 class="text-center">~ Em contrução ~</h4>
-    </div>
+        <?php if($pedidos != "Ocorreu um erro."){ ?>
+        <div class="panel panel-default">
+            <div class="panel-heading bg-primary">
+                <h3 class="text-center" style="margin: 0">Tabela de <?= count($pedidos) ?> Pedido(s)
+                </h3>
+            </div>
+                <div class="table-responsive prod">
+                    <table class="table table-striped prod">
+                        <thead>
+                            <tr>
+                                <th class="text-center">ID</th>
+                                <th class="text-center">Data de Emissão</th>
+                                <th class="text-center">Valor Total</th>
+                                <th class="text-center">Status</th>
+                                <th class="text-center">Cliente</th>
+                                <th class="text-center">Visualizar</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php foreach($pedidos as $pedido => $atual){ ?>
+                            <tr>
+                                <td class="text-center"><b><?= $atual->getCdPedido() ?></b></td>
+                                <td class="text-center"><b><?= $atual->getDtEmissao() ?></b></td>
+                                <td class="text-center"><b><?= $atual->getVlTotalPedido() ?></b></td>
+                                <td class="text-center"><b><?= $atual->getIcCancelado() ?></b></td>
+                                <td class="text-center"><b><?= $atual->getCdCliente() ?></b></td>
+                                <td class="text-center">
+                                    <button class="btn btn-primary btn-lg" data-toggle="modal" data-target="#produtoPedidoEdit<?= $row ?>">
+                                        <i class="glyphicon glyphicon-pencil" style="vertical-align: text-top;"></i>
+                                    </button>
+                                </td>
+                            </tr>
+                            <?php } ?>
+                        </tbody>
+                    </div>
+                </table>
+            </div>
+            <?php }else{ ?>
+                <h1 class="text-center">
+                    <b>Não há pedidos no momento.<br>
+                    <a href="<?= HOME_PATH . '/home/admin' ?>">Clique aqui</a> para voltar</b>
+                </h1>
+            <?php } ?>
+        </div>
+        
