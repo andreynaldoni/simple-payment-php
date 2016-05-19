60915807d52e88ac6a494be81ae7f23f70baebe2
Andrey Naldoni <andreynaldoni@gmail.com>
2016-05-19 17:10:44 -0300

    Production Config

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
     // Session Intialize
     session_start();
-	//$db_url = parse_url(getenv('JAWSDB_URL')); // Production Environment
-    $db_url = parse_url(getenv('MYSQL_LOCAL_DB')); // Local Development Environment
+	$db_url = parse_url(getenv('JAWSDB_URL')); // Production Environment
+    //$db_url = parse_url(getenv('MYSQL_LOCAL_DB')); // Local Development Environment
     
     define('DB_HOSTNAME', $db_url['host']);
 	define('DB_USERNAME', $db_url['user']);
@@ -11,8 +11,8 @@
 	define('DB_PREFIX','tb');
 	define('DB_CHARSET','utf8');
     // URL
-    //define('HOME_PATH', 'https://php-payment.herokuapp.com'); // Production Path
-	define('HOME_PATH', 'http://localhost/simple-payment-php'); // Local Development Path
+    define('HOME_PATH', 'https://php-payment.herokuapp.com'); // Production Path
+	//define('HOME_PATH', 'http://localhost/simple-payment-php'); // Local Development Path
     // Application
 	require_once 'app.php';
     // PHP_ENV ? production : development
