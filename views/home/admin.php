4d75c15477407af0e2479f23d6029d937279fe3e
Andrey Henriques Naldoni <andreynaldoni@gmail.com>
2016-05-19 12:41:56 -0300

    Admin icons

--- views/home/admin.php
+++ views/home/admin.php
@@ -5,35 +5,31 @@
 ?>
 <div class="container">
     <div class="row">
-        <div class="col-md-6">
-            <a href="<?= HOME_PATH ?>/cliente/index" class="btn btn-primary btn-block btn-large">
-                <h3 class="text-center" style="margin: 0">Clientes
-                <br>
-                <img class="img-rounded" src="<?= image_show('/admin/clientes.png') ?>" alt="" width="175" height="175"></h3>
+        <div class="col-sm-6 text-center">
+            <h2>Clientes</h2>
+            <a href="<?= HOME_PATH ?>/cliente/index" class="btn btn-large btn-info">
+                <img class="img-rounded" style="padding: 15px" src="<?= image_show('/admin/clientes.png') ?>" alt="" width="150" height="150">
             </a>
         </div>
-        <div class="col-md-6">
-            <a href="<?= HOME_PATH ?>/produto/index" class="btn btn-primary btn-block btn-large">
-                <h3 class="text-center" style="margin: 0">Produtos
-                <br>
-                <img class="img-rounded" src="<?= image_show('/admin/produtos.png') ?>" alt="" width="175" height="175"></h3>
+        <div class="col-sm-6 text-center">
+            <h2>Produtos</h2>
+            <a href="<?= HOME_PATH ?>/produto/index" class="btn btn-large btn-success">
+                <img class="img-rounded" style="padding: 15px" src="<?= image_show('/admin/produtos.png') ?>" alt="" width="150" height="150">
             </a>
         </div>
     </div>
     <br>
     <div class="row">
-        <div class="col-md-6">
-            <a href="<?= HOME_PATH ?>/ingrediente/index" class="btn btn-primary btn-block btn-large">
-                <h3 class="text-center" style="margin: 0">Ingredientes
-                <br>
-                <img class="img-rounded" src="<?= image_show('/admin/ingredientes.png') ?>" alt="" width="175" height="175"></h3>
+        <div class="col-sm-6 text-center">
+            <h2>Ingredientes</h2>
+            <a href="<?= HOME_PATH ?>/ingrediente/index" class="btn btn-large btn-warning">
+                <img class="img-rounded" style="padding: 15px" src="<?= image_show('/admin/ingredientes.png') ?>" alt="" width="150" height="150">
             </a>
         </div>
-        <div class="col-md-6">
-            <a href="<?= HOME_PATH ?>/pedido/index" class="btn btn-primary btn-block btn-large">
-                <h3 class="text-center" style="margin: 0">Pedidos
-                <br>
-                <img class="img-rounded" src="<?= image_show('/admin/pedidos.png') ?>" alt="" width="175" height="175"></h3>
+        <div class="col-sm-6 text-center">
+            <h2>Pedidos</h2>
+            <a href="<?= HOME_PATH ?>/pedido/index" class="btn btn-large btn-danger">
+                <img class="img-rounded" style="padding: 15px" src="<?= image_show('/admin/pedidos.png') ?>" alt="" width="150" height="150">
             </a>
         </div>
     </div>
